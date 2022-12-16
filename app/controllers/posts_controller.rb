ced7d09a8d0cde1fe490ff3f4095c2cd5c67c471
toto-maru009 <cyborg.kohei002@outlook.jp>
2022-12-17 00:05:18 +0900

    コントローラーにリクエストを送信する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.order(id: "DESC")
   end
-
+ 
+   # コメントアウトする
+   # def new
+   # end
+ 
   def create
     Post.create(content: params[:content])
-    redirect_to action: index
-
-end
+    redirect_to action: :index  
+  end
+ 
+ end
+ 
